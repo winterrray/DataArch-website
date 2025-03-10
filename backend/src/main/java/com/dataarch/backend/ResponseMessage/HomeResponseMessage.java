d9720a35ead1df7b9922de5306aee56474ef5b69
Arya Rajvaidya <aryarajvaidya@MacBook-Pro-2.local>
2025-03-10 21:58:17 +0530

    update

---
+++ backend/src/main/java/com/dataarch/backend/ResponseMessage/HomeResponseMessage.java
@@ -0,0 +1,14 @@
+package com.dataarch.backend.ResponseMessage;
+
+import com.dataarch.backend.models.HomePage.Home;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HomeResponseMessage {
+    Home Home;
+}
